57980b8e6f63c333339c02c8205abdaac25be874
Gregor Kookmaa <gregor.kookmaa@tptlive.ee>
2021-08-02 19:36:00 +0300

    Added 'Posted by' and 'Commented by' visual to pages

--- html/blog/resources/views/post.blade.php
+++ html/blog/resources/views/post.blade.php
@@ -9,12 +9,15 @@
                 {{$post->content}}
             </div>
             <div class="card-footer text-muted">
+                <p>Posted By: {{\App\Models\User::find($post->user_id )->name}}</p>
                 {{$post->created_at->diffForHumans()}}
             </div>
         </div>
         @foreach ($post->comments as $comment)
             <div class="alert alert-dark mt-2">
                 {{$comment->content}}
+                <p></p>
+                <p>Comment from - {{\App\Models\User::find($comment->user_id )->name}}</p>
             </div>
         @endforeach
 @endsection
